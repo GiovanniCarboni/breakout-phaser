c21f4c12a57bca23f26821da1dcdbedb959c6b3d
Giovanni Carboni <giovanni.carboni@trinaware.com>
2023-11-30 19:16:36 +0200

    Fix configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/scripts/main.ts",
@@ -18,6 +20,15 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "index.html",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "./public/assets", to: "assets" }],
+    }),
+  ],
   devServer: {
     static: path.join(__dirname, "public"),
     compress: true,
