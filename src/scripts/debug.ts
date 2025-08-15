a608fe93c3cf83c9b97124b297935d40c694ae23
GiovanniCarboni <giovanni.carboni.1997@gmail.com>
2025-08-15 17:59:53 +0200

    Improve debug

--- src/scripts/debug.ts
+++ src/scripts/debug.ts
@@ -1,5 +1,33 @@
-export const debug = {
+type DebugKeys = keyof typeof debug
+const debug = {
     physics: false,
     fireBall: false,
     cannons: false,
+    holdBall: false,
+    shortPaddle: false, // either short or long, cant be both
+    longPaddle: false,
 }
+
+const displayDebugging = () => {
+    if (!Object.values(debug).filter(x => !!x).length) return
+    const debugTableData = Object.keys(debug).map((behavior: string) => ({
+        behavior: behavior,
+        debugging: debug[behavior as DebugKeys]
+    }))
+    const style = `
+        color: orangered;
+        font-size: .9rem;
+        padding: .2rem .8rem;
+        font-weight: bold;
+        border-radius: 5px;
+        background-color: yellow;
+        margin: .4rem;
+    `
+    console.group("%c Debug Settings", style)
+    console.table(debugTableData)
+    console.groupEnd()
+}
+displayDebugging()
+
+
+export { debug }
